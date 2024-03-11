67f562d20dc6805962a5682f92632befcbb72207
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-11 20:35:34 +0300

    argocd enabled

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,9 +1,9 @@
-# resource "helm_release" "argocd" {
-#   name = "argocd"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd"
-#   create_namespace = true
-#   version          = "6.6.0"
-#   values = [file("argocdvalues.yaml")]
-# }
+resource "helm_release" "argocd" {
+  name = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "6.6.0"
+  values = [file("argocdvalues.yaml")]
+}
