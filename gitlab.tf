8ad3b8ec80f5d81c7c521c711ebe8cc320441e40
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-16 18:20:44 +0300

    fix

--- gitlab.tf
+++ gitlab.tf
@@ -12,31 +12,31 @@ resource "helm_release" "gitlab" {
   namespace  = "gitlab"
   
 
-#   values = [file("${path.module}/values/gitlab.yaml")]
-#   depends_on = [ kubernetes_namespace.gitlab-namespace, resource.kubectl_manifest.gitlab-passwd-token] 
-#   timeout = "600"
-#   set {
-#     name  = "timeout"
-#     value = "600s"
-#   }
+  values = [file("${path.module}/values/gitlab.yaml")]
+  depends_on = [ kubernetes_namespace.gitlab-namespace, resource.kubectl_manifest.gitlab-passwd-token] 
+  timeout = "600"
+  set {
+    name  = "timeout"
+    value = "600s"
+  }
+}
+
+resource "kubectl_manifest" "gitlab-passwd-token" {
+    count     = length(data.kubectl_filename_list.gitlab-passwd-token.matches)
+    yaml_body = file(element(data.kubectl_filename_list.gitlab-passwd-token.matches, count.index))
+}
+
+data "kubectl_filename_list" "gitlab-passwd-token" {
+    pattern = "./manifests/gitlab-passwd-token.yaml"
+}
+
+# data "terracurl_request" "test" {
+#   name   = "check"
+#   url    = "http://gitlab.minikube.local"
+#   method = "GET"
+#   skip_tls_verify = true
+#   response_codes = [200]
+
+#   max_retry      = 3
+#   retry_interval = 10
 # }
-
-# resource "kubectl_manifest" "gitlab-passwd-token" {
-#     count     = length(data.kubectl_filename_list.gitlab-passwd-token.matches)
-#     yaml_body = file(element(data.kubectl_filename_list.gitlab-passwd-token.matches, count.index))
-# }
-
-# data "kubectl_filename_list" "gitlab-passwd-token" {
-#     pattern = "./manifests/gitlab-passwd-token.yaml"
-# }
-
-# # data "terracurl_request" "test" {
-# #   name   = "check"
-# #   url    = "http://gitlab.minikube.local"
-# #   method = "GET"
-# #   skip_tls_verify = true
-# #   response_codes = [200]
-
-# #   max_retry      = 3
-# #   retry_interval = 10
-# # }
